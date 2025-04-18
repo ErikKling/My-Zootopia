989ec1ce5e31f1e5bb198794d355ec3a28ef81c9
Erik Kling <kling.erik@icloud.com>
2025-04-18 17:23:12 +0200

    Add safer key access and handle output

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,9 +7,22 @@ def load_data(file_path):
 
 animals_data = load_data('animals_data.json')
 
-for animal in animals_data:
-    print(f"Name: {animal["name"]}")
-    print(f"Diet: {animal["characteristics"]["diet"]}")
-    print(f"Location: {animal["locations"][0]}")
-    print(f"Type: {animal["characteristics"]["type"]}")
-    print()
+def print_data_from_file():
+
+    for animal in animals_data:
+        name = animal.get("name")
+        diet = animal.get("characteristics", {}).get("diet")
+        locations = animal.get("locations", [])
+        location = locations[0] if locations else None
+        type = animal.get("characteristics", {}).get("type")
+
+        if name:
+            print(f"Name: {name}")
+        if diet:
+            print(f"Diet: {diet}")
+        if location:
+            print(f"Location: {location}")
+        if type:
+            print(f"Type: {type}")
+
+        print()
