aad8c544a8cd2daa5bbbc7c3749cf26894daeb9b
Erik Kling <kling.erik@icloud.com>
2025-04-19 21:00:00 +0200

    Refactor: generate card layout with <li> and <ul> structure

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,7 +7,8 @@ def load_data(file_path):
 
 animals_data = load_data('animals_data.json')
 
-def print_data_from_file():
+def get_data_from_file():
+    html_content = ""
 
     for animal in animals_data:
         name = animal.get("name")
@@ -16,13 +17,30 @@ def print_data_from_file():
         location = locations[0] if locations else None
         type = animal.get("characteristics", {}).get("type")
 
-        if name:
-            print(f"Name: {name}")
-        if diet:
-            print(f"Diet: {diet}")
-        if location:
-            print(f"Location: {location}")
-        if type:
-            print(f"Type: {type}")
 
-        print()
+        html = '<li class="cards__item">\n'
+
+        if name:
+            html += f"Name: {name}<br>\n"
+        if diet:
+            html += f"Diet: {diet}<br>\n"
+        if location:
+            html += f"Location: {location}<br>\n"
+        if type:
+            html += f"Type: {type}<br>\n"
+        
+        html += '</li>\n'
+
+        html_content += html
+    
+    return f'<ul class="cards">{html_content}</ul>'
+
+html_content = get_data_from_file()
+
+with open('animals_template.html', "r") as template_file:
+    template = template_file.read()
+
+final_html = template.replace("__REPLACE_ANIMALS_INFO__", html_content)
+
+with open('animals_page.html', "w") as output_file:
+    output = output_file.write(final_html)
